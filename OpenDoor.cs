33f32a1e03aaed01d0db38fd76ff09ee73e7d549
Offlinee <42832031+Offlinee@users.noreply.github.com>
2019-01-24 09:24:18 +0100

    Add files via upload

---
+++ OpenDoor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OpenDoor : MonoBehaviour
+{
+    public string LevelToLoad = "Level 1";
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            SceneManager.LoadScene(LevelToLoad);
+        }
+    }
+}
